4abf44ad261fdf4c3ed74bae0a1fa6a39434c937
felipejoq <felipejofreq@gmail.com>
2025-07-22 11:07:17 -0400

    feat(product): implement product page with size selector and not found component

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,9 +1,55 @@
-export default function ProductPage() {
+import { SizeSelector } from "@/components";
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+    params: Promise<{ slug: string }>;
+}
+
+export default async function ProductPage({ params }: Props) {
+
+    const { slug } = await params;
+
+    const product = initialData.products.find(product => product.slug === slug);
+
+    if (!product) {
+        notFound();
+    }
+
+
+
     return (
-        <div>
-            <h1 className="bg-blue-400 min-h-screen">
-                Product Page "asdasda"
-            </h1>
+        <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+            {/* Slideshow */}
+            <div className="col-span-1 md:col-span-2">
+                <h1>SlideShow</h1>
+            </div>
+            {/* Details */}
+            <div className="col-span-1 px-5">
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+                    {product.title}
+                </h1>
+                <p className="text-lg mb-5">${product.price}</p>
+
+                {/* Selector de tallas */}
+                <SizeSelector selectedSize={product.sizes[0]} availableSizes={product.sizes} />
+
+                {/* Selector de cantidades */}
+
+                {/* Button add to cart */}
+                <button className="btn-primary my-5">
+                    Agregar al carrito
+                </button>
+
+                {/* Description */}
+                <h3 className="font-bold text-sm">
+                    Descripción
+                </h3>
+                <p className="font-light">
+                    {product.description}
+                </p>
+            </div>
         </div>
     );
 }
