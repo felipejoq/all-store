3b8c1e1afc0235dedd9583e9aa88e1972f9fedda
felipejoq <felipejofreq@gmail.com>
2025-08-07 18:05:31 -0400

    feat: enhance AddToCart component with size and quantity state management, and update SizeSelector and QuantitySelector for improved functionality

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,24 +1,33 @@
 'use client'
 
 import { QuantitySelector, SizeSelector } from "@/components"
-import { Product } from "@/interfaces";
+import { Product, Size } from "@/interfaces";
+import { useState } from "react";
 
 interface Props {
     product: Product;
 }
 
 export const AddToCart = ({ product }: Props) => {
+    const [size, setSize] = useState<Size | undefined>();
+    const [quantity, setQuantity] = useState<number>(1);
+
     return (
         <>
             {/* Selector de tallas */}
             <SizeSelector
-                selectedSize={product.sizes[0]}
+                selectedSize={size}
                 availableSizes={product.sizes}
+                onSizeChanged={setSize}
             />
 
             {/* Selector de cantidades */}
             <h3 className="font-bold mb-4">Cantidad</h3>
-            <QuantitySelector quantity={2} />
+            <QuantitySelector
+                stock={product.inStock}
+                quantity={quantity}
+                onQuantityChanged={setQuantity}
+            />
 
             {/* Button add to cart */}
             <button className="btn-primary my-5 cursor-pointer">
